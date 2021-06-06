967d6571761372173a7b0a7c2d2ccf393b9ca1c3
Diego Ricardo <diegoricardoweb@gmail.com>
2021-06-05 22:10:03 -0300

    Add netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -4,5 +4,16 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+const axios = require('axios')
+const netlifyWebhook = strapi.config.get("custom.netlifyWebhook")
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook && axios.post(netlifyWebhook);
+    },
+    async afterUpdate(_, __, ___) {
+      netlifyWebhook && axios.post(netlifyWebhook);
+    }
+  }
+};
