99381bbbb052b65828df05db106552a0a17b3cdc
Diego Ricardo <diegoricardoweb@gmail.com>
2021-06-05 21:38:46 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e16a84b44809e991017b476d31ae8807'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c08becac-6f04-4e7e-a567-fb22640317ea'
+};
